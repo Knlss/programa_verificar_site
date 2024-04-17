fec2ff9ae9a7f6837f204334b92a8a4be13c50aa
Kauan <kauanleosilva2008@gmail.com>
2024-04-17 17:54:05 -0300

    update

--- temporario/gui/utils/utils_gui.py
+++ temporario/gui/utils/utils_gui.py
@@ -21,12 +21,41 @@ class CreateFrame:
             self.frame.pack(side=height, fill=width, expand=anchor)
         else:
             raise ValueError("Tipo de frame não suportado: {}".format(frame_type))
+        
+# ---------------------------------------------------------------------------------------------------------
 
 class CreateElement:
-    def __init__(self, father, height, width, anchor, relx, rely, bg, element_type):
-        pass
 
+    def create_button(self, father, height, width, anchor, relx, rely, bg, fg, font, text, command):
+        self.frame = tk.Button(father, height=height, width=width, text=text, font=font, bg=bg, fg=fg, command=command)
+        self.frame.place(anchor=anchor, relx=relx, rely=rely)
 
+    def create_checkbutton(self, **info):
+        self.frame = tk.Button(self.father, **info)
+
+    def create_input(self, **info):
+        self.frame = tk.Label(self.father, **info)
+    
+    def create_label(self, **info):
+        self.frame = tk.Label(self.father, **info)
+    
+# ---------------------------------------------------------------------------------------------------------
+
+"""
+
+        Vou fazer o seguinte: seguindo a mesma lógica do createFrame, irei executar um metodo por vez
+    informando uma lista de cada tipo por vez. Ela vai receber o mesmo tratamento da lista de frame info
+    obtendo o pai com .pop e definindo numa variavel. Essa variavel vai ser usada para obter o valor do 
+    pai do elemento atual que esta contido na lista de frames, e depois vai instanciar em cadeia um novo
+    elemento, usando o metodo especifico do tipo, e passando a variavel father como pai, e **info como o 
+    resto das informações. Nisso, numa outra lista chamada elements, que no caso é um dicionario, esse 
+    elemento recem criado vai ser armazenado com .frame, ou seja, vai armazenar o proprio objeto tkinter,
+    e como chave, vai ter seu nome que foi definido na lista de info do seu tipo particular
+
+        Vou usar 4 dicionarios de dicionarios diferentes, um para botao, outro label, outro checkbutton, outro
+    input. Acaba que terei que fazer +4 for in, alem do for in dos frames.
+
+"""
 
 def create_window(largura, altura):
     root = tk.Tk()
